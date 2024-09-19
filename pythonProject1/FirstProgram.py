bd8b559989b94041fe6e33ab1424fb41967e1fde
vermasuman <flowergcet@gmail.com>
2024-09-18 18:30:18 -0700

    Pysample
    new file added here

---
+++ pythonProject1/FirstProgram.py
@@ -0,0 +1,71 @@
+a=(5, 88, 12)
+for i in a:
+    print(i)
+#user define methods
+
+def sample():
+    print("hello world")
+    print("hi friends")
+sample()
+
+def sum(x,y):
+    print(x+y)
+    print(x*y)
+sum(30,45)
+
+def subtract(a,b):
+    return a-b
+print(subtract(45,25))
+"""
+x=20
+print(x)
+print(type(x))
+
+x=30
+if x>0:
+    print("positive")
+elif x<0:
+    print("negative")
+else:
+    print("nutral")
+
+i=0
+while i<=5:
+    print(i)
+    j=20
+    while j<= 22:
+        print(j)
+        j=j+1
+    i=i+1
+print("hello friends")
+
+
+#name=input("Enter your name: ")
+#print("Hello " + name)
+"""
+""" triple cotes for multiple line comment
+1. variable
+2. conditional
+3. loop
+4. arrays / list
+5. user defined methods
+
+conversion function
+int()
+float()
+str()
+bool()
+
+a = int(input("Enter first number : "))
+b= int(input("Enter second number : "))
+print(a+b)
+
+flag=False
+flag1=True
+name = "Lalitha"
+name ='Sam'
+birth_year=input("Enter your year of birth : ")
+age =2024 -int(birth_year)
+print("your age: ", age)
+
+"""
