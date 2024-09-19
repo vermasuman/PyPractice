9c67883651c996c73dae8e0d3a508e18ba75ef12
vermasuman <flowergcet@gmail.com>
2024-09-19 13:42:02 -0700

    updated Parent and child file
    Add child and parent file for inheritance

---
+++ pythonProject1/Child.py
@@ -0,0 +1,15 @@
+import sre_compile
+
+from Parent import Bank
+class Savings(Bank):
+    acct_type = ""
+    def __init__(self, ac_type, fn, ln):
+        self.acct_type = ac_type
+        super().__init__(fn,ln)
+
+    def display(self):
+        print(self.first_name+' '+self.last_name)
+        print("Account Type: "+self.acct_type)
+
+s1=Savings("CD Account","Amit", "Agarwal")
+s1.display()
